bc10014941843964d42685303715e2b4e63e5ef6
MobileDev <absomech123.com>
2024-04-05 12:00:03 +0500

    init commit

--- tastytoast/build.gradle.kts
+++ tastytoast/build.gradle.kts
@@ -1,5 +1,10 @@
+import org.gradle.api.JavaVersion
+import org.gradle.api.publish.maven.MavenPublication
+import org.gradle.kotlin.dsl.create
+
 plugins {
     id("com.android.library")
+    id("maven-publish")
 }
 
 android {
@@ -8,7 +13,6 @@ android {
 
     defaultConfig {
         minSdk = 21
-
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         consumerProguardFiles("consumer-rules.pro")
     }
@@ -22,14 +26,35 @@ android {
             )
         }
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
+
+    publishing {
+        singleVariant("release") {
+            withSourcesJar()
+            withJavadocJar()
+        }
+    }
+}
+
+publishing {
+    publications {
+        register<MavenPublication>("release") {
+            groupId = "tasty-toast"
+            artifactId = "tasty-toast"
+            version = "0.0.1"
+
+            afterEvaluate {
+                from(components["release"])
+            }
+        }
+    }
 }
 
 dependencies {
-
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     testImplementation("junit:junit:4.13.2")
